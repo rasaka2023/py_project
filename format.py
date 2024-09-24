6cf8f234a72158c5f34dd3780eccdfae21efb2da
rasaka2023 <rasaka69@gmail.com>
2024-09-24 18:05:24 -0500

    Initial commit

---
+++ format.py
@@ -0,0 +1,12 @@
+
+#Defining my variables to that will be used with format() function
+first_name= "SAKA"
+age= 105
+favorite_color= "Dark_Brown"
+
+# Leveraging format() function to Print out my code 
+print("Hello my name is {}. I am {} years old. My favorite color is {}.".format(first_name, age, favorite_color))
+
+
+
+
