aaecda35d1b1c88158baa51c66beb917af7cad93
rasaka2023 <151390917+rasaka2023@users.noreply.github.com>
2024-09-24 19:17:30 -0500

    Add files via upload

---
+++ fun.py
@@ -0,0 +1,13 @@
+# This is a varibale for the city name Nassaua , which the captial of Bahamas
+city= "Nassua"
+# This a variable for price in floating number
+price= "56.9"
+# This is a variable for high score in Int
+high_score = 99
+# This is varibale for is_having_fun in boolean
+is_having_fun= True
+
+print(city)
+print(price)
+print(high_score)
+print(is_having_fun)
