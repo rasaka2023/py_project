85e2cf58255d634d1395c33867a2aaffc59917b8
rasaka2023 <151390917+rasaka2023@users.noreply.github.com>
2024-09-30 19:13:57 -0500

    Add files via upload

---
+++ sum_calculator.py
@@ -0,0 +1,15 @@
+# Asking or prompting the user to enter an integer number
+number1 = int(input("Please enter an integer number: "))
+# Printing out the number that will be entered 
+print(f"You entered: {number1}")
+
+# Asking or prompting the user to enter a float number
+number2 = float(input("Please enter an floating number:"))
+# Print the float number that will be entered
+print(f"You entered: {number2}")
+
+# Define a variable name sum
+sum = number1 + number2
+
+# Print the result/ addition of the variable above
+print(sum)
