d74ae593b0867395a92a7cb3d7d7dbd7bbc13b3b
rasaka2023 <kasara69@gmail.com>
2024-10-03 01:53:16 -0500

    Added my fun file

---
+++ fun.py
@@ -0,0 +1,23 @@
+# This is a varibale for the city name Nassaua , which the captial of Bahamas
+city= "Nassua"
+
+# This a variable for price in floating number
+price= "56.9"
+
+# This is a variable for high score in Int
+high_score = 99
+# This store the integer into string for me so that I can use in the print function concentenation
+high_score_as_string = str(high_score)
+
+# This is varibale for is_having_fun in boolean
+is_having_fun= True
+# Tis store boolean into string for me so that I can use in the print function concentenation
+is_having_fun= str(is_having_fun)
+
+#print(city)
+#print(price)
+#print(high_score)
+#print(is_having_fun)
+
+# IKeep take way I was not able to print the con=mbination of String, Boolean, Integer, this why I re-store varivale to strings
+print(city + " " + price + " " +  high_score_as_string + " " + is_having_fun)
